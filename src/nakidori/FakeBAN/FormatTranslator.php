e156a8dd68dd0207a49be4c843d1d595a4864e07
Nakidori <92979288+Nakidori@users.noreply.github.com>
2022-03-14 00:52:30 +0900

    FormatTranslatorを編集

--- src/nakidori/FakeBAN/FormatTranslator.php
+++ src/nakidori/FakeBAN/FormatTranslator.php
@@ -6,11 +6,8 @@ class FormatTranslator {
     
     public static function translate(String $format, $array) : string{
         $parameters = self::getParameters($format);
-        $key = 0;
-        foreach ($parameters as $param) {
-            var_dump($param[0]." => ".$array[$key]);
+        foreach ($parameters as $key => $param) {
             $format = str_replace($param[0], $array[$key] ?? "", $format);
-            $key++;
         }
         return $format;
     }
